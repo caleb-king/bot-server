9014a58749a7fea9f3ce3f29dba7230b64ed5696
Caleb King <caleb.king536@gmail.com>
2020-04-09 10:39:49 -0600

    prepared for heroku deployment

---
+++ src/app.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const bodyParser = express.json();
+
+const app = express();
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'dev';
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+
+app.put('/', bodyParser, (req, res) => {
+  global.command = req.body.command;
+  res.end();
+});
+
+app.get('/', (req, res) => {
+  res.status(200).send(global.command);
+});
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
+module.exports = app;
